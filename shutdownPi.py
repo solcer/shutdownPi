4bd82af660fdb16dd70058919528b795d2ebc774
solcer <olcerselim@gmail.com>
2014-03-28 21:12:23 +0000

    shutdown komutu while dongunun disina aldim.

--- shutdownPi.py
+++ shutdownPi.py
@@ -10,6 +10,7 @@ import os
 
 
 def shutdown(buton):
+	shutdown=0
 	while True:
 		mybutton = GPIO.input(buton)					#buton degerini okuyoruz.
 		if mybutton == False:	
@@ -18,8 +19,9 @@ def shutdown(buton):
 			print "Baslangic: " + str(t0) 
 			while GPIO.input(buton) == False:				#butona basýlý tutulduðu sürece bekliyoruz
 				t1 = time.clock()					#t1'e yeni zamaný alýyoruz
-				if t1-t0>9 :
-					os.system("sudo shutdown -h now")
+				if t1-t0>5 :						# kapatilacak sure
+					shutdown=1					#buradan çýkýp raspberry pi yi kapatýyoruz
+					break
 				if t1-fark>1 :					#
 					print str(t1-t0) +"sn"
 					fark=t1					#geçen süredeki farký bulmak için fark registerini güncelliyorum.
@@ -28,6 +30,10 @@ def shutdown(buton):
 			print t0
 			print "t1: "
 			print t1
+	if shutdown=1:
+		print "bye bye..."
+		os.system("sudo shutdown -h now")
+
 		
 buton = 11										#Buton'un baglanacagi pin numarasi
 GPIO.setmode(GPIO.BOARD)
