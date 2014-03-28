0d511c8eecdfed7c7e3286e1836e17a032921d34
solcer <olcerselim@gmail.com>
2014-03-28 21:39:36 +0000

    yeni branch olusturdum. Butona bir kere basinca raspi kapanir.

--- shutdownPi.py
+++ shutdownPi.py
@@ -10,31 +10,8 @@ import os
 
 
 def shutdown(buton):
-	shutdown=0
-	while True:
-		mybutton = GPIO.input(buton)					#buton degerini okuyoruz.
-		if mybutton == False:	
-			t0 = time.clock()						#süre tutmak için t0'a o anki zamaný alýyoruz.
-			fark=t0
-			print "Baslangic: " + str(t0) 
-			while GPIO.input(buton) == False:				#butona basýlý tutulduðu sürece bekliyoruz
-				t1 = time.clock()					#t1'e yeni zamaný alýyoruz
-				if t1-t0>5 :						# kapatilacak sure
-					shutdown=1					#buradan çýkýp raspberry pi yi kapatýyoruz
-					break
-				if t1-fark>1 :					#
-					print str(t1-t0) +"sn"
-					fark=t1					#geçen süredeki farký bulmak için fark registerini güncelliyorum.
-			t1 = time.clock()						#t1'e yeni zamaný alýyoruz
-			print "t0: "
-			print t0
-			print "t1: "
-			print t1
-		if shutdown==1:							#while True'dan cikmak icin
-			break
-	if shutdown==1:
-		print "bye bye..."
-		os.system("sudo shutdown -h now")
+	print "bye bye..."
+	os.system("sudo shutdown -h now")
 
 		
 buton = 11										#Buton'un baglanacagi pin numarasi
